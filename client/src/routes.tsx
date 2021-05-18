2cee0ff0663db2b3d82896896627f7125c303d27
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-18 10:43:03 -0700

    Created simple unauthorized route for testing paths and redirections

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,19 +1,21 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
+import ProtectedRoute from './protectedRoute';
+import Unauthorized from './unauthorized';
 
 const Routes = (): JSX.Element => (
   <>
     <Switch>
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
-      <Route exact path="/dashboard">
-        <Dashboard />
-      </Route>
-      <Route path="*">
+      <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/unauthorized" component={Unauthorized} />
+      {/* Need to re-access the way this redirec occurs as it happens with useAuth's useEffect (check useAuth.tsx) */}
+      {/* <Route path="*">
         <Redirect to="/login" />
-      </Route>
+      </Route> */}
     </Switch>
   </>
 );
