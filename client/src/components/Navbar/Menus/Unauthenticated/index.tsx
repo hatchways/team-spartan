d8b5de27ff6ce65182db253c2479a73c4ce08b0c
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-05-20 19:45:12 -0700

    Fix for unnecessary history by using useLocation instead

--- client/src/components/Navbar/Menus/Unauthenticated/index.tsx
+++ client/src/components/Navbar/Menus/Unauthenticated/index.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
 import { Grid } from '@material-ui/core';
-import { useHistory } from 'react-router';
+import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import useStyles from './useStyles';
 
 export const index = (): JSX.Element => {
   const classes = useStyles();
-  const history = useHistory();
-  const [location, setLocation] = React.useState<string>(history.location.pathname);
-
-  console.log('history:', history.location.pathname);
+  const location = useLocation();
 
   return (
     <Grid item>
-      <Link
-        className={classes.button}
-        to={location === '/login' ? '/signup' : '/login'}
-        onClick={() => setLocation(location === '/login' ? '/signup' : '/login')}
-      >
-        {location === '/login' ? 'SIGN UP' : 'SIGN IN'}
+      <Link className={classes.button} to={location.pathname === '/login' ? '/signup' : '/login'}>
+        {location.pathname === '/login' ? 'SIGN UP' : 'SIGN IN'}
       </Link>
     </Grid>
   );
